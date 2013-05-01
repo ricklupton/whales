51f55591ba4dde120608af1f5aaf116fa0c0339c
Rick Lupton <r.lupton@gmail.com>
2013-05-01 12:16:30 +0100

    Start to reorganise whales code and model

---
+++ whales/hydrodynamics.py
@@ -0,0 +1,85 @@
+"""
+Hydrodynamic calculations -- force spectra etc
+"""
+
+import numpy as np
+from numpy import newaxis
+from scipy import integrate, constants
+from scipy.interpolate import interp1d
+
+from whales.utils import shift, response_spectrum
+
+
+class HydrodynamicsInfo(object):
+    def __init__(self, first_order_data, second_order_data=None):
+        self.first_order_data = first_order_data
+        self.second_order_data = second_order_data
+
+        # Make interpolators
+        self.A = interp1d(first_order_data.w, first_order_data.A, axis=0)
+        self.B = interp1d(first_order_data.w, first_order_data.B, axis=0)
+        self.C = self.wamit.C
+        self.X = interp1d(first_order_data.w, first_order_data.X, axis=0)
+
+        # QTFs
+        if second_order_data is not None:
+            self.Tc = interp1d(second_order_data.w,
+                               second_order_data.Tc, axis=0)
+        else:
+            self.Tc = None
+
+    def wave_drift_damping(self, w, S_wave):
+        """Calculate wave-drift damping coefficient"""
+        Tc = self.Tc(w)
+        # Only include surge and sway
+        # XXX is this right? do I need to treat sway differently?
+        dTc_dw = np.gradient(Tc, w[1] - w[0])[0]  # gradient along axis 0
+
+        integrand = (S_wave[:, newaxis] *
+                     (4 * w[:, newaxis] * Tc + w[:, newaxis]**2 * dTc_dw) /
+                     constants.g)
+        B12 = integrate.simps(integrand, w, axis=0)
+
+        B = np.zeros((6, 6))
+        B[0, 0] = B12[0]
+        B[1, 1] = B12[1]
+        return B
+
+    def first_order_force_spectrum(self, w, S_wave, heading=0):
+        ih = np.nonzero(self.wamit.headings == heading)[0][0]
+        # Interpolate complex wave excitation force
+        X = self.X(w)[:, ih, :]
+        return response_spectrum(X, S_wave)
+
+    def second_order_force_spectrum(self, w, S_wave, heading=0):
+        """Calculate the 2nd order force spectrum defined by
+        $$ S_i^{(2)}(\omega) =
+        8 \int S_{\eta\eta}(\mu) S_{\eta\eta}(\mu + \omega) \,
+        \left| T_i^{\mathrm{c}}(\mu, \mu + \omega) \right|^2 \mathrm{d}\mu $$
+        but extended to give the cross-spectrum S_ij
+        """
+
+        if heading != 0:
+            raise RuntimeError("No QTF data for headings other than 0")
+
+        # First make the matrix SS_kl, where l is the freq and k is the offset
+        #  SS_kl = S(w_l) * S(w_l + w_k)
+        SS = np.tile(S_wave, (len(w), 1))
+        for k in range(len(w)):
+            SS[k, :] *= shift(S_wave, k, fill=0)
+
+        # Next use the Newman approximation to make a similar matrix TTc
+        Tc = self.Tc(w)  # interpolate -> (len(w), 6) matrix
+        TTc = np.tile(Tc, (len(w), 1, 1))  # copy downwards new 0 axis
+        for k in range(len(w)):
+            for i in range(6):
+                TTc[k, :, i] += shift(Tc[:, i], k)
+        TTc /= 2  # average
+
+        # Now loop through each DOF to make the 2nd order spectrum
+        S2 = np.zeros((len(w), 6, 6))
+        for i in range(6):
+            for j in range(6):
+                integrand = SS * TTc[:, :, i] * TTc[:, :, j]
+                S2[:, i, j] = 8 * integrate.simps(integrand, w, axis=1)
+        return S2
---
+++ whales/io.py
@@ -0,0 +1,110 @@
+"""
+Tools for loading WAMIT output data
+"""
+
+import numpy as np
+from numpy import newaxis, pi
+import h5py
+from scipy import constants
+
+class WAMITData(object):
+    def __init__(self, path, skiprows=0, water_density=1e3):
+        """Load WAMIT results data from the run ``path``:
+         - The hydrodynamic added-mass matrix ``A``
+         - The hydrodynamic radiation damping matrix ``B``
+         - The hydrostatic restoring force matrix ``C``
+         - The wave excitation forces ``X``
+
+        ``A`` and ``B`` are frequency-dependent matrices of shape
+        (frequency, 6, 6). ``C`` is a constant matrix of shape (6, 6).
+        ``X`` is a frequency- and heading-dependent vector of shape
+        (frequency, heading, 6).
+
+        These quantities are denormalised using the supplied value
+        for water density, and standard gravitational acceleration.
+        """
+        self.path = path
+        self.water_density = water_density
+
+        # Radiation data:
+        #      A: Added mass
+        #      B: Radiation damping
+        #      T: Wave period (s)
+        #   i, j: motion indices
+        rad = np.genfromtxt(self.path + '.1', skiprows=skiprows,
+                            names=('T', 'i', 'j', 'A', 'B'),
+                            delimiter=(14, 6, 6, 14, 14))
+
+        # Wave excitation data
+        ex = np.genfromtxt(self.path + '.3', skiprows=skiprows,
+                           names=('T', 'head', 'i', 'mod', 'pha', 're', 'im'))
+
+        # Restoring force data
+        rest = np.genfromtxt(self.path + '.hst', skiprows=skiprows,
+                             names=('i', 'j', 'C'), comments='!')
+
+        # Get set of periods and headings
+        period_list = sorted(set(rad['T']) | set(ex['T']))
+        heading_list = sorted(set(ex['head']))
+        periods = np.array(period_list)
+        headings = np.array(heading_list)
+
+        # Rearrange the data
+        self.A = np.zeros((len(periods), 6, 6))
+        self.B = np.zeros((len(periods), 6, 6))
+        for row in rad:
+            ip = period_list.index(row['T'])
+            self.A[ip, row['i']-1, row['j']-1] = row['A']
+            self.B[ip, row['i']-1, row['j']-1] = row['B']
+
+        self.X = np.zeros((len(periods), len(headings), 6), dtype=np.complex)
+        for row in ex:
+            ip = period_list.index(row['T'])
+            ih = heading_list.index(row['head'])
+            self.X[ip, ih, row['i']-1] = row['re'] + 1j*row['im']
+
+        self.C = np.zeros((6, 6))
+        for row in rest:
+            self.C[row['i']-1, row['j']-1] = row['C']
+
+        # Re-dimensionalise
+        self.A *= water_density
+        self.B *= water_density * (2 * pi / periods[:, newaxis, newaxis])
+        self.C *= water_density * constants.g
+        self.X *= water_density * constants.g
+
+        # Re-arrange into frequency order (not period order)
+        # make smallest period very small instead of zero
+        w = 2 * np.pi / periods
+        w[periods == -1] = 0
+        w[periods == 0] = np.inf
+
+        # sort by frequency not period
+        idx = np.argsort(w)
+        self.w = w[idx]
+        self.headings = headings
+        self.A = self.A[idx]
+        # Damping goes to nan as w -> 0
+        self.B = np.nan_to_num(self.B[idx])
+        self.X = self.X[idx]
+
+
+class SecondOrderData(object):
+    def __init__(self, path, water_density=1e3):
+        """
+        Load pre-processed QTF diagonal values from HDF5 file
+        """
+        self.path = path
+        with h5py.File(path) as f:
+            w = f['diff/diag/w'][...]
+            Tc = f['diff/diag/Tc'][...]
+
+        # De-normalise
+        Tc *= water_density * constants.g
+        if w[0] != 0:
+            print "Assuming low-frequency asymptote of zero"
+            w = np.r_[0, w]
+            Tc = np.r_[np.zeros((1, 6)), Tc]
+
+        self.w = w
+        self.Tc = Tc
