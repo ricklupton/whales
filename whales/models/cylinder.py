54d684a770cc963fcca6e6dd70e87a67a8e68862
Rick Lupton <r.lupton@gmail.com>
2013-07-10 14:33:32 +0100

    Update check vs Drake2011 for finite water depth

--- whales/models/cylinder.py
+++ whales/models/cylinder.py
@@ -36,29 +36,32 @@ def Morison_added_mass(w, draft, radius, Cm=2.0):
     return LinearSystem(A, zeros_like(A), zeros_like(A))
 
 
-def first_order_excitation(k, draft, radius):
-    """Returns F/(rho g a^2 A) -- from Drake"""
+def first_order_excitation(k, draft, radius, water_depth):
+    """Returns F/(rho g a^2 A) -- from Drake, but adapted by me to
+    integrate down to the draft depth only (not to the seabed)"""
     ka = k * radius
     kd = k * draft
+    kh = k * water_depth
 
     # XXX check this!
     f1 = -1j * (jn(1, ka) - jnd(1, ka) * hankel2(1, ka) / hankel2d(1, ka))
-    M = (-2 * pi / ka) * f1 * (1 - cosh(kd)) / (k * cosh(kd))
-    F = (-2 * pi / ka) * f1 * sinh(kd) / cosh(kd)
+    M = (kd*sinh(kh-kd) + cosh(kh-kd) - cosh(kh)) / (k * cosh(kh))
+    F = (-sinh(kh-kd) + sinh(kh)) / cosh(kh)
     X = np.zeros(M.shape + (6,), dtype=np.complex)
-    X[..., 0] = F
-    X[..., 4] = M
+    X[..., 0] = (-2*pi/ka) * f1 * F
+    X[..., 4] = (-2*pi/ka) * f1 * M
     return X
 
 
-def excitation_force(w, draft, radius):
+def excitation_force(w, draft, radius, water_depth):
     """Excitation force on cylinder using Drake's first_order_excitation"""
     k = w**2 / 9.81
-    X = 1025 * 9.81 * radius**2 * first_order_excitation(k, draft, radius)
+    X = 1025 * 9.81 * radius**2 * first_order_excitation(k, draft, radius,
+                                                         water_depth)
     return X
 
 
-def mean_surge_force(w, depth, radius, RAOs, N=10):
+def mean_surge_force(w, depth, radius, RAOs, N=10, deep_water=False):
     """Mean surge force on a surging and pitching cylinder
     From Drake2011.
     """
@@ -72,13 +75,57 @@ def mean_surge_force(w, depth, radius, RAOs, N=10):
     hankel_term = lambda n, ka: 1 - ((hankel1d(n, ka) * hankel2d(n-1, ka)) /
                                      (hankel2d(n, ka) * hankel1d(n-1, ka)))
     terms = np.nansum([hankel_term(n, ka) for n in range(1, N)], axis=0)
-    incoming_part = (1 + (2*kd/sinh(2*kd))) * terms
+    if deep_water:
+        incoming_part = 1 * terms
+    else:
+        incoming_part = (1 + (2*kd/sinh(2*kd))) * terms
 
     # This is the part which depends on the first-order motion
     hankel_term2 = (hankel2d(0, ka) / hankel1d(0, ka) +
                     hankel2d(2, ka) / hankel1d(2, ka))
-    motion_part = 2j * ((RAOs[:, 4] * (1 - cosh(kd)) / k +
-                         RAOs[:, 0] * sinh(kd)) /
-                        (cosh(kd) * hankel2d(1, ka)) * hankel_term2)
+    if deep_water:
+        motion_part = 2j * ((RAOs[:, 0] - (RAOs[:, 4] / k)) *
+                            hankel_term2 / hankel2d(1, ka))
+    else:
+        motion_part = 2j * ((RAOs[:, 4] * (1 - cosh(kd)) / k +
+                             RAOs[:, 0] * sinh(kd)) /
+                            (cosh(kd) * hankel2d(1, ka)) * hankel_term2)
+
+    return 0.5 / ka * np.real(incoming_part + motion_part)
+
+
+def mean_surge_force2(w, depth, radius, RAOs, N=10, deep_water=False):
+    """Mean surge force on a surging and pitching cylinder
+    From Drake2011.
+    """
+
+    surge_rao = RAOs[:, 0] - depth*RAOs[:, 4]
+    pitch_rao = RAOs[:, 4]
+
+    g = 9.81
+    k = w**2 / g     # XXX deep water
+    ka = k * radius
+    kd = k * depth
+
+    # This is the part of the force which depends on the incoming waves
+    hankel_term = lambda n, ka: 1 - ((hankel1d(n, ka) * hankel2d(n-1, ka)) /
+                                     (hankel2d(n, ka) * hankel1d(n-1, ka)))
+    terms = np.nansum([hankel_term(n, ka) for n in range(1, N)], axis=0)
+    if deep_water:
+        incoming_part = 1 * terms
+    else:
+        incoming_part = (1 + (2*kd/sinh(2*kd))) * terms
+
+    # This is the part which depends on the first-order motion
+    hankel_term2 = (hankel2d(0, ka) / hankel1d(0, ka) +
+                    hankel2d(2, ka) / hankel1d(2, ka))
+    if deep_water:
+        motion_part = 2j * ((surge_rao + pitch_rao * depth * (1 - (1/kd))) *
+                            hankel_term2 / hankel2d(1, ka))
+    else:
+        motion_part = 2j * ((pitch_rao * (1 - cosh(kd) + kd*sinh(kd)) +
+                             surge_rao * k * sinh(kd)) /
+                            (k * cosh(kd) * hankel2d(1, ka))
+                            * hankel_term2)
 
     return 0.5 / ka * np.real(incoming_part + motion_part)
